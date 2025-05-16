5a0f5e1de6fa6c0edd427586274983c8f4b305af
devjohnpark <devjohnpark@gmail.com>
2025-05-16 16:06:48 +0900

    [Remove]

--- http-web-server/src/main/java/org/dochi/http/request/stream/Http11RequestStream.java
+++ http-web-server/src/main/java/org/dochi/http/request/stream/Http11RequestStream.java
@@ -15,10 +15,8 @@ public class Http11RequestStream implements HttpCrlfLineReader, HttpBodyReader {
     private static final int LF = '\n';  // Line Feed
     private final InputStream in;
     private final ByteArrayOutputStream lineBuffer = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream lineBuffer = new ByteArrayOutputStream();
 
     public Http11RequestStream(InputStream in) {
-//        super(in);
         this.in = in;
     }
 
@@ -57,42 +55,6 @@ public class Http11RequestStream implements HttpCrlfLineReader, HttpBodyReader {
         sizeMonitor.monitorSize(actualContentLength); // 라인 크기 전달
         return body;
     }
-
-//    // lineBuffer를 사용하면 또 데이터를 중복 저장해야 하므로 SocketBufferedInputStream에서 가져온다.
-//    private byte[] readLine(int limitLineSize) throws IOException {
-//        lineBuffer.reset();
-//        int previousByte = -1;
-//        int currentByte;
-//
-//        while ((currentByte = read()) != -1) {
-//
-//            lineBuffer.write(currentByte);
-//
-//            // CR+LF 조합을 찾으면 줄의 끝
-//            if (previousByte == CR && currentByte == LF) {
-//                // 마지막 CRLF 문자 제거
-//                return trimBuffer(lineBuffer, lineBuffer.size() - 2);
-//            }
-//
-//            // line과 부합할때까지 읽은 데이터 크기가 커지는것을 방지
-//            // lineBuffer: 12345\r1 (7)
-//            // limitLineSize: 5
-//            if (lineBuffer.size() >= limitLineSize + 2) {
-//                throw new LineTooLongIOException("Read line size exceeds the limit bytes", limitLineSize);
-//            }
-//
-//            previousByte = currentByte;
-//        }
-//
-//        // Close-Wait으로 인한 EOF: 스트림의 끝에 도달했고 아무것도 읽지 못했다면 null 반환 (currentByte == -1 && lineBuffer.size() == 0)
-//        if (lineBuffer.size() == 0) {
-//            return null;
-//        }
-//
-//        throw new NotFoundCrlfIOException("Unexpected end of stream for missing CRLF", lineBuffer.toString(StandardCharsets.UTF_8));
-//    }
-
-
 
     // lineBuffer를 사용하면 또 데이터를 중복 저장해야 하므로 SocketBufferedInputStream에서 가져온다.
     private byte[] readLine(int limitLineSize) throws IOException {
