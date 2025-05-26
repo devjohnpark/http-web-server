4bcc183f06600dd1e50563b8adc575f88d27c25c
devjohnpark <devjohnpark@gmail.com>
2025-05-26 17:51:17 +0900

    [Test] add MessageBytesTest

---
+++ http-web-server/src/test/java/org/dochi/http/data/multipart/PartTest.java
@@ -0,0 +1,34 @@
+package org.dochi.http.data.multipart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartTest {
+
+
+
+    @Test
+    void getFileName() {
+    }
+
+    @Test
+    void getContentType() {
+    }
+
+    @Test
+    void isFile() {
+    }
+
+    @Test
+    void getContent() {
+    }
+
+    @Test
+    void removeFile() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+}
