5d557c662a6424d4cc4a2ab8a8ec9ee7d201c195
devjohnpark <devjohnpark@gmail.com>
2025-05-26 18:05:13 +0900

    [Test] add MultipartFileManagerTest

--- http-web-server/src/test/java/org/dochi/http/data/multipart/PartTest.java
+++ http-web-server/src/test/java/org/dochi/http/data/multipart/PartTest.java
@@ -1,34 +1,39 @@
-package org.dochi.http.data.multipart;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PartTest {
-
-
-
-    @Test
-    void getFileName() {
-    }
-
-    @Test
-    void getContentType() {
-    }
-
-    @Test
-    void isFile() {
-    }
-
-    @Test
-    void getContent() {
-    }
-
-    @Test
-    void removeFile() {
-    }
-
-    @Test
-    void isEmpty() {
-    }
-}
+//package org.dochi.http.data.multipart;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import java.io.File;
+//import java.util.UUID;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class PartTest {
+//
+//    Part part = new Part();
+//
+//
+//    @Test
+//    void getFileName() {
+//
+//    }
+//
+//    @Test
+//    void getContentType() {
+//    }
+//
+//    @Test
+//    void isFile() {
+//    }
+//
+//    @Test
+//    void getContent() {
+//    }
+//
+//    @Test
+//    void removeFile() {
+//    }
+//
+//    @Test
+//    void isEmpty() {
+//    }
+//}
