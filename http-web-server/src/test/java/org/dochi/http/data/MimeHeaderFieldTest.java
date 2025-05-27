e0a4dcdf88b7f56b4b829b0c25f6925363b949ee
devjohnpark <devjohnpark@gmail.com>
2025-05-27 11:23:51 +0900

    [Test] add MimeHeaderFieldTest

---
+++ http-web-server/src/test/java/org/dochi/http/data/MimeHeaderFieldTest.java
@@ -0,0 +1,47 @@
+package org.dochi.http.data;
+
+import org.dochi.internal.buffer.MessageBytes;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MimeHeaderFieldTest {
+
+    @Test
+    void initialNameAndValueAreEmpty() {
+        MimeHeaderField field = new MimeHeaderField();
+        assertTrue(field.getName().isNull());
+        assertTrue(field.getValue().isNull());
+    }
+
+    @Test
+    void nameAndValueCanBeSetAndRetrieved() {
+        MimeHeaderField field = new MimeHeaderField();
+        field.getName().setString("Content-Type");
+        field.getValue().setString("text/html");
+
+        assertEquals("Content-Type", field.getName().toString());
+        assertEquals("text/html", field.getValue().toString());
+    }
+
+    @Test
+    void recycleClearsNameAndValue() {
+        MimeHeaderField field = new MimeHeaderField();
+        field.getName().setString("Host");
+        field.getValue().setString("localhost");
+
+        field.recycle();
+
+        assertTrue(field.getName().isNull());
+        assertTrue(field.getValue().isNull());
+    }
+
+    @Test
+    void toStringReturnsFormattedHeader() {
+        MimeHeaderField field = new MimeHeaderField();
+        field.getName().setString("Accept");
+        field.getValue().setString("application/json");
+
+        assertEquals("Accept: application/json", field.toString());
+    }
+}
